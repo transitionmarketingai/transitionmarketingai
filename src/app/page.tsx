0ec2b4e30e05525ba20177766fb3937ec3af116d
transitionmarketingai <abhishekjohn@transitionmarketingai.com>
2025-10-01 16:38:56 +0530

    Fix build error: Move animations to global CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -565,28 +565,6 @@ export default function HomePage() {
         </div>
       </footer>
 
-      <style jsx>{`
-        @keyframes blob {
-          0% {
-            transform: translate(0px, 0px) scale(1);
-          }
-          33% {
-            transform: translate(30px, -50px) scale(1.1);
-          }
-          66% {
-            transform: translate(-20px, 20px) scale(0.9);
-          }
-          100% {
-            transform: translate(0px, 0px) scale(1);
-          }
-        }
-        .animate-blob {
-          animation: blob 7s infinite;
-        }
-        .animation-delay-2000 {
-          animation-delay: 2s;
-        }
-      `}</style>
     </div>
   );
 }
